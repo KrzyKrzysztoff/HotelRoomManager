065561bf4e5fcb31ddd12be5db06b6747bf2cd01
Krzysiek Jaworski <krzysztof.jaworski98@gmail.com>
2025-02-13 19:41:26 +0100

    add models

--- HotelRoomManager.Domain/Models/Room.cs
+++ HotelRoomManager.Domain/Models/Room.cs
@@ -8,5 +8,12 @@ namespace HotelRoomManager.Domain.Models
 {
     public class Room
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Size { get; set; }
+        public RoomStatus Status { get; set; }
+        public string? Description { get; set; }
+        public AvailabilityDetail? Detail { get; set; }
     }
+   
 }
---
+++ HotelRoomManager.Domain/Models/RoomStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomManager.Domain.Models
+{
+    public enum RoomStatus
+    {
+        Available,
+        Occupied,
+        Cleaning,
+        Maintenance,
+        ManuallyLocked
+    }
+}
