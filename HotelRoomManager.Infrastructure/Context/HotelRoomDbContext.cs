e9cd737bea78cc8291503344a50bee8fa172b0a3
Krzysiek Jaworski <krzysztof.jaworski98@gmail.com>
2025-02-13 18:55:08 +0100

    init

---
+++ HotelRoomManager.Infrastructure/Context/HotelRoomDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelRoomManager.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelRoomManager.Infrastructure.Context
+{
+    public class HotelRoomDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Room> Rooms{ get; set; }
+    }
+}
